fca13b154a0d3d8fd2751e8a2588ce5079aaa0f7
babafemiolatona <femiolatona@gmail.com>
2025-07-05 17:05:30 +0100

    feat: add Swagger configuration for API documentation

---
+++ src/main/java/com/finance/expense_tracker/swagger/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.finance.expense_tracker.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Currency Converter API")
+                        .version("1.0")
+                        .description("Real-time currency conversion using external APIs"));
+    }
+
+}
